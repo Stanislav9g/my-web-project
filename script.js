c5e53b19dd0ac82558767ca84288520f8b02b84d
Stanislav9g <ryzhenkostas2001@mail.ru>
2026-04-09 18:31:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,40 @@ const modal = document.getElementById("presModal");
 const btn = document.getElementById("openPres");
 const span = document.getElementsByClassName("close")[0];
 const slides = document.querySelectorAll(".slide");
+const counter = document.getElementById("slideCounter");
 let currentSlide = 0;
 
-btn.onclick = () => modal.style.display = "block";
+// Функция обновления счетчика
+function updateCounter() {
+    counter.innerText = `${currentSlide + 1} / ${slides.length}`;
+}
+
+// Открытие и закрытие
+btn.onclick = () => {
+    modal.style.display = "block";
+    updateCounter();
+};
 span.onclick = () => modal.style.display = "none";
 
+// Закрытие по клику вне окна
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Следующий слайд
 document.getElementById("nextSlide").onclick = () => {
     slides[currentSlide].classList.remove("active");
     currentSlide = (currentSlide + 1) % slides.length;
     slides[currentSlide].classList.add("active");
+    updateCounter();
 };
 
+// Предыдущий слайд
 document.getElementById("prevSlide").onclick = () => {
     slides[currentSlide].classList.remove("active");
     currentSlide = (currentSlide - 1 + slides.length) % slides.length;
     slides[currentSlide].classList.add("active");
+    updateCounter();
 };
